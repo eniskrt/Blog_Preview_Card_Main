56947aee6f78e9631edf1b6f98c30220748271b8
eniskrt <enskerti@gmail.com>
2023-12-30 01:12:51 +0300

    my first react repo

---
+++ src/components/card.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Badge, Card } from 'react-bootstrap'
+import "./style.scss"
+
+const MainCard = () => {
+  return (
+    <Card className='card'>
+      <Card.Body>
+      <Card.Img variant="top" className='img' src="./images/illustration-article.svg" />
+      <h5><Badge className='badge text-dark'>Learning</Badge></h5>
+      <Card.Text className='subBadge'>Published 21 Dec 2023</Card.Text>
+        <Card.Title><h1>HTML &amp; CSS foundations</h1></Card.Title>
+        <Card.Text className='text'>
+        These languages are the backbone of every website, defining structure, content, and presentation.
+        </Card.Text>
+        <div className='sub-card'>
+            <img src="./images/image-avatar.webp" alt="" />
+            <h5>Greg Hooper</h5>
+        </div>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default MainCard
